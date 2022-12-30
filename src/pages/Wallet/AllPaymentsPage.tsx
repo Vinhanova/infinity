0cbb0433df133e822042079b7cd2ad06c4c67f05
Vinhanova <tiagovinhanova@gmail.com>
2022-12-30 19:34:46 +0000

    Firestore connection

--- src/pages/Wallet/AllPaymentsPage.tsx
+++ src/pages/Wallet/AllPaymentsPage.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../firebase'
 
 type Props = {}
 
+type Payment = {
+  id: string
+  title: string
+  price: number
+  category: string
+  date: string
+}
+
 const AllPaymentsPage = (props: Props) => {
-  return <div>AllPaymentsPage</div>
+  const [listAllPayments, setListAllPayments] = useState<Array<Payment>>([])
+
+  useEffect(() => {
+    const q = query(collection(db, 'payments'))
+
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      let arrAllPayments: Array<Payment> = []
+
+      querySnapshot.forEach(doc => {
+        arrAllPayments.push({
+          id: doc.id,
+          title: doc.data().title,
+          price: doc.data().price,
+          category: doc.data().category,
+          date: new Date(doc.data().date.seconds * 1000).toLocaleString('pt-PT', { timeZone: 'UTC' })
+        })
+      })
+
+      setListAllPayments(arrAllPayments!)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  return (
+    <>
+      {console.log(listAllPayments)}
+      <div>AllPaymentsPage</div>
+      {listAllPayments?.map((payment: Payment) => {
+        console.log(payment)
+        return <h1>{payment.date}</h1>
+      })}
+    </>
+  )
 }
 
 export default AllPaymentsPage
