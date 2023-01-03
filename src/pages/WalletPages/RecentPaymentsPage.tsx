08fd06ab395b978213d8132c1f3dd12de7fd4693
Vinhanova <tiagovinhanova@gmail.com>
2023-01-03 15:05:13 +0000

    Simple prototype Layout

--- src/pages/WalletPages/RecentPaymentsPage.tsx
+++ src/pages/WalletPages/RecentPaymentsPage.tsx
@@ -3,7 +3,34 @@ import React from 'react'
 type Props = {}
 
 const RecentPaymentsPage = (props: Props) => {
-  return <div>RecentPaymentsPage</div>
+  return (
+    <>
+      <div className='m-16'>
+        <div className='flex gap-x-6'>
+          <div className='h-80 w-3/4 border-2 border-white'>Calendar</div>
+          <div className='flex w-1/4 justify-center border-2 border-white text-center'>
+            <div className='w-full'>
+              <h1>Media</h1>
+              <p>132</p>
+              <p>103</p>
+              <p>125</p>
+              <p>117</p>
+              <p>60</p>
+            </div>
+            <div className='w-full justify-center border-l-2 border-white'>
+              <h1>Saldo</h1>
+              <p>132</p>
+              <p>103</p>
+              <p>125</p>
+              <p>117</p>
+              <p>60</p>
+            </div>
+          </div>
+        </div>
+        <div className='mt-6 h-16 border-2 border-white'>Total</div>
+      </div>
+    </>
+  )
 }
 
 export default RecentPaymentsPage
