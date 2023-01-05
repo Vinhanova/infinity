d5261179747f92ebda3e2f6669afe7e9ecbb8ac6
Vinhanova <tiagovinhanova@gmail.com>
2023-01-05 23:58:16 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,5 @@ export default defineConfig({
   plugins: [react(), EnvironmentPlugin(['VITE_API_KEY', 'VITE_AUTH_DOMAIN', 'VITE_PROJECT_ID', 'VITE_STORAGE_BUCKET', 'VITE_MESSAGING_SENDER_ID', 'VITE_APP_ID', 'VITE_MEASUREMENT_ID'])],
   build: {
     chunkSizeWarningLimit: 1600
-  },
-  resolve: {
-    alias: {
-      path: 'dotenv'
-    }
   }
 })
