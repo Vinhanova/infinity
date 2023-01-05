1b249aeb523a5f4941a3f665750cac23090901b2
Vinhanova <tiagovinhanova@gmail.com>
2023-01-05 23:38:56 +0000

    Deploy test

---
+++ public/env.js
@@ -0,0 +1,9 @@
+window.env = {
+  "VITE_API_KEY": "AIzaSyBWVKQhsonBKdV49aik4w1jVXwRkz23dPg",
+  "VITE_AUTH_DOMAIN": "infinity-28ac1.firebaseapp.com",
+  "VITE_PROJECT_ID": "infinity-28ac1",
+  "VITE_STORAGE_BUCKET": "infinity-28ac1.appspot.com",
+  "VITE_MESSAGING_SENDER_ID": "81840423940",
+  "VITE_APP_ID": "1:81840423940:web:1851bf21a1311892d1a151",
+  "VITE_MEASUREMENT_ID": "G-MVRXZTHMS0"
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin(['VITE_API_KEY', 'VITE_AUTH_DOMAIN', 'VITE_PROJECT_ID', 'VITE_STORAGE_BUCKET', 'VITE_MESSAGING_SENDER_ID', 'VITE_APP_ID', 'VITE_MEASUREMENT_ID'])],
   build: {
     chunkSizeWarningLimit: 1600
+  },
+  resolve: {
+    alias: {
+      path: 'dotenv'
+    }
   }
 })
