f717fec99c6f64ee5667cc5f159cf4d597179bd3
Vinhanova <tiagovinhanova@gmail.com>
2023-01-06 00:33:18 +0000

    test

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,9 +8,8 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const apiKey = process.env.VITE_API_KEY
 const firebaseConfig = {
-  apiKey,
+  apiKey: process.env.VITE_API_KEY,
   authDomain: process.env.VITE_AUTH_DOMAIN,
   projectId: process.env.VITE_PROJECT_ID,
   storageBucket: process.env.VITE_STORAGE_BUCKET,
