5577f5e5a28f0a8fa1685c641d0f6bd03af2654e
Vinhanova <tiagovinhanova@gmail.com>
2023-01-06 17:28:04 +0000

    EnvironmentPlugin removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin(['VITE_API_KEY', 'VITE_AUTH_DOMAIN', 'VITE_PROJECT_ID', 'VITE_STORAGE_BUCKET', 'VITE_MESSAGING_SENDER_ID', 'VITE_APP_ID', 'VITE_MEASUREMENT_ID'])],
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600
   }
