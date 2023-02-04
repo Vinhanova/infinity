fb42aefb1eb3693a1494e41e70824c554cce497c
Vinhanova <tiagovinhanova@gmail.com>
2023-02-04 22:33:23 +0000

    Total calculation

--- src/pages/InvestmentsPages/InvestmentsPage.tsx
+++ src/pages/InvestmentsPages/InvestmentsPage.tsx
@@ -10,6 +10,7 @@ const InvestmentsPage: FC = () => {
   const { user } = UserAuth()
   const { state, data: userStocks, error } = useDocFirestore<userStocks>(`stocks`, user.uid)
   const [stocks, setStocks] = useState<{}>({})
+  const [total, setTotal] = useState<number>(0)
 
   useEffect(() => {
     if (userStocks) {
@@ -44,27 +45,42 @@ const InvestmentsPage: FC = () => {
     }
   }, [userStocks])
 
+  useEffect(() => {
+    setTotal(+_.reduce(stocks, (total: number, stock: any) => total + stock.price * userStocks![stock.id].quantity, 0).toFixed(2))
+  }, [stocks])
+
   return (
     <>
       <div className='flex w-full flex-col items-center'>
         <div className='mt-8 w-3/4'>
-          {state === 'pending' && <h1>Pending</h1>}
-          {state === 'error' && <h1>{error.toString()}</h1>}
-          {state === 'success' && _.isEmpty(stocks) ? (
-            <h1>0 stocks</h1>
-          ) : (
-            _.map(stocks, (stock: any) => (
-              <div key={stock.id} className='mb-5 w-full border-2'>
-                <p>{userStocks![stock.id].name + ' (' + stock.id + ')'}:</p>
-                <div className='flex justify-between'>
-                  <p className={stock.changePercent > 0 ? 'text-green-500' : 'text-red-500'}>
-                    {stock.price.toFixed('2')} ({stock.changePercent.toFixed(2)}%)
-                  </p>
-                  <p className='mr-2'>{(stock.price * userStocks![stock.id].quantity).toFixed(2)} $</p>
-                </div>
-              </div>
-            ))
-          )}
+          <div>
+            {state === 'pending' && <h1>Pending</h1>}
+            {state === 'error' && <h1>{error.toString()}</h1>}
+            {state === 'success' && _.isEmpty(stocks) ? (
+              <h1>0 stocks</h1>
+            ) : (
+              _.map(stocks, (stock: any) => {
+                return (
+                  <div key={stock.id} className='mb-5 w-full border-2'>
+                    <div className='flex justify-between'>
+                      <p>{userStocks![stock.id].name + ' (' + stock.id + ')'}:</p>
+                      <p className={stock.changePercent > 0 ? 'text-green-500' : 'text-red-500'}>
+                        {stock.price.toFixed('2')} ({stock.changePercent.toFixed(2)}%)
+                      </p>
+                    </div>
+                    <p className='text-right'>{(stock.price * userStocks![stock.id].quantity).toFixed(2)} $</p>
+                  </div>
+                )
+              })
+            )}
+          </div>
+          <div className='flex justify-between'>
+            <h1>Total: </h1>
+            <div>
+              <h1>{total} $</h1>
+              <h1>{(total * 0.92).toFixed(2)} €</h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
