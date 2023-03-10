02fe94564cd30be271f10cbd5c1a45d57ac0427f
Vinhanova <tiagovinhanova@gmail.com>
2023-03-10 12:52:37 +0000

    PieChart more colors added

--- src/pages/InvestmentsPages/PieChart.tsx
+++ src/pages/InvestmentsPages/PieChart.tsx
@@ -17,8 +17,10 @@ const PieChart: FC = () => {
         data: _.map(stocksInfoData, stock => {
           return (stock.price * userTickersData![stock.id].quantity * exchangeRateInfoData.c).toFixed(2)
         }),
-        backgroundColor: ['#2A2B2E', '#007991', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+        /* '#2A2B2E', '#007991' */
+
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(75, 192, 100, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(200, 250, 0, 0.2)', 'rgba(250, 250, 200, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 100, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(200, 250, 0, 1)', 'rgba(250, 250, 200, 1)'],
         borderWidth: 1
       }
     ]
