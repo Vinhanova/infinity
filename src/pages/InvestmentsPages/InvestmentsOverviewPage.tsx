5dcf3839790e18c30d737f10bafc082769221917
Vinhanova <tiagovinhanova@gmail.com>
2023-03-10 20:16:30 +0000

    Minor change

--- src/pages/InvestmentsPages/InvestmentsOverviewPage.tsx
+++ src/pages/InvestmentsPages/InvestmentsOverviewPage.tsx
@@ -9,7 +9,7 @@ const InvestmentsOverviewPage: FC = () => {
   return (
     <div className='flex items-center justify-center'>
       <div className='mt-8 flex h-full w-full flex-col items-center justify-center gap-8 xs:w-10/12 sm:w-8/12 md:w-6/12 xl:w-4/12'>
-        <div className='flex w-full justify-between'>
+        <div className='flex w-full justify-between px-4 xs:px-0'>
           <div className='text-left'>
             <h1>Total: </h1>
             <h1>(USD/EUR: {exchangeRateInfoData?.c.toFixed(3)})</h1>
