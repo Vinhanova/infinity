e81281ef06917ce1c40029b731b9df4d9cdb16b2
Vinhanova <tiagovinhanova@gmail.com>
2023-03-12 19:16:57 +0000

    InvestmentsOVPage error/loading handling

--- src/pages/InvestmentsPages/InvestmentsOverviewPage.tsx
+++ src/pages/InvestmentsPages/InvestmentsOverviewPage.tsx
@@ -1,28 +1,53 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import _ from 'underscore'
 import { useInvestmentsContext } from '../../Context/InvestmentsContext'
 import { toFixed } from '../../utils/utils'
 import PieChart from './PieChart'
 
 const InvestmentsOverviewPage: FC = () => {
-  const { totalUSD, totalEUR, exchangeRateInfoData } = useInvestmentsContext()
+  const { listState, initialTickersInfoError, stocksInfoError, stocksInfoData, totalUSD, totalEUR, exchangeRateInfoData } = useInvestmentsContext()
 
   return (
-    <div className='flex items-center justify-center'>
-      <div className='my-8 flex h-full w-full flex-col items-center justify-center gap-8 xs:w-10/12 sm:w-8/12 md:w-6/12 xl:w-4/12'>
-        <div className='flex w-full justify-between px-4 xs:px-0'>
-          <div className='text-left'>
-            <h1>Total: </h1>
-            <h1>(USD/EUR: {exchangeRateInfoData?.c.toFixed(3)})</h1>
+    <div className='my-4 w-full text-center sm:my-8'>
+      {listState === 'pending' && <h1 className='mb-8'>Loading...</h1>}
+
+      {listState === 'error' &&
+        ((initialTickersInfoError?.response?.status === 429 && <h1 className='mb-8 text-red-500'>Warning: Slow Down (429)</h1>) ||
+          (stocksInfoError === 'No tickers found' && (
+            <div className='mb-8'>
+              <h3>
+                0 assets found, you can add a new asset{' '}
+                <Link to='/investments/new-asset' className='underline'>
+                  here
+                </Link>
+                .
+              </h3>
+            </div>
+          )))}
+
+      {listState === 'success' &&
+        (_.isEmpty(stocksInfoData) ? (
+          <h1 className='p-2'>No stocks found</h1>
+        ) : (
+          <div className='flex items-center justify-center'>
+            <div className='flex h-full w-full flex-col items-center justify-center gap-8 xs:w-10/12 sm:w-8/12 md:w-6/12 xl:w-4/12'>
+              <div className='flex w-full justify-between px-4 xs:px-0'>
+                <div className='text-left'>
+                  <h1>Total: </h1>
+                  <h1>(USD/EUR: {exchangeRateInfoData?.c.toFixed(3)})</h1>
+                </div>
+                <div className='text-right'>
+                  <h1>{totalUSD} $</h1>
+                  <h1>{toFixed(totalEUR, 2)} €</h1>
+                </div>
+              </div>
+              <div className='flex w-full items-center justify-center'>
+                <PieChart />
+              </div>
+            </div>
           </div>
-          <div className='text-right'>
-            <h1>{totalUSD} $</h1>
-            <h1>{toFixed(totalEUR, 2)} €</h1>
-          </div>
-        </div>
-        <div className='flex w-full items-center justify-center'>
-          <PieChart />
-        </div>
-      </div>
+        ))}
     </div>
   )
 }
