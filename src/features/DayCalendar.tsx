e9fd70dfce6e0ecf65e6e9366af1517ead7e7693
Vinhanova <tiagovinhanova@gmail.com>
2023-08-13 13:35:37 +0100

    Minor layout changes

--- src/features/DayCalendar.tsx
+++ src/features/DayCalendar.tsx
@@ -19,7 +19,7 @@ const DayCalendar: FC<Props> = ({ day, dailyPaymentsList }) => {
         rows.push(
           <button onClick={() => alert(moment.unix(dailyPaymentsList[i].date.seconds).format('hh:mm:ss'))} className='mb-1 w-[90%] rounded-md bg-custom-tealblue px-2 group-hover:bg-custom-jet/50 group-hover:hover:bg-slate-200 group-hover:hover:text-custom-jet'>
             <div key={i} className='flex justify-between'>
-              <h4>{dailyPaymentsList[i].title}</h4>
+              <h4 className='font-semibold'>{dailyPaymentsList[i].title}</h4>
               <h4>{dailyPaymentsList[i].price} €</h4>
             </div>
           </button>
@@ -38,7 +38,7 @@ const DayCalendar: FC<Props> = ({ day, dailyPaymentsList }) => {
 
   return (
     <div className={`${day.outOfBounds && 'bg-zinc-800 text-slate-400'} group rounded bg-custom-jet hover:bg-custom-tealblue`}>
-      <h3 className='text-lg'>{day.nr}</h3>
+      <h3 className='text-lg font-bold'>{day.nr}</h3>
       <div className='max-h-[80%] w-full items-center justify-center overflow-auto'>{printFirst3Rows()}</div>
     </div>
   )
