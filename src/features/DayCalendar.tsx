ba1d3ea8d815d6e29943c9c2df140c89bbb8f3c8
Vinhanova <tiagovinhanova@gmail.com>
2023-08-15 09:41:57 +0100

    Revert "Overflow iPad Fix"
    This reverts commit 3233ec6627ecdd9d623a95f0c9f7474a109465d9.

--- src/features/DayCalendar.tsx
+++ src/features/DayCalendar.tsx
@@ -38,9 +38,9 @@ const DayCalendar: FC<Props> = ({ day, dailyPaymentsList }) => {
 
   return (
     <div className='border-2 border-zinc-800'>
-      <div className={`group h-full w-full overflow-auto rounded hover:bg-custom-tealblue ${day.outOfBounds ? 'bg-zinc-800 text-slate-400' : 'bg-custom-jet'}`}>
+      <div className={`group h-full w-full rounded hover:bg-custom-tealblue ${day.outOfBounds ? 'bg-zinc-800 text-slate-400' : 'bg-custom-jet'}`}>
         <h3 className='text-lg font-bold'>{day.nr}</h3>
-        <div className='max-h-[80%] w-full items-center justify-center'>{printFirst3Rows()}</div>
+        <div className='max-h-[80%] w-full items-center justify-center overflow-auto'>{printFirst3Rows()}</div>
       </div>
     </div>
   )
