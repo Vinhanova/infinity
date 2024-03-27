b24f27a7724049a2b8d90e26d6f10433c6cc57d5
Vinhanova <tiagovinhanova@gmail.com>
2024-03-27 12:32:05 +0000

    LoginPage layout adjustments

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -32,16 +32,16 @@ const LoginPage: FC = () => {
   return (
     <>
       <div className='flex min-h-screen flex-col items-center justify-items-center font-mono'>
-        <div className='mt-4 flex min-h-[60vh] w-full flex-col-reverse justify-center sm:flex-row sm:items-center sm:px-16'>
-          <div className='flex h-full flex-col justify-items-center gap-y-6 p-4 text-center sm:m-0 sm:w-5/12 sm:p-0 sm:text-start lg:pl-4 xl:pl-24'>
-            <h1 className='text-2xl sm:text-5xl'>
-              Projeto <IoInfiniteSharp className='mb-1 inline text-3xl sm:mb-2 sm:text-5xl' /> Infinity
+        <div className='mt-4 flex w-full flex-col-reverse justify-center sm:flex-row sm:items-center sm:px-8 md:px-16 portrait:min-h-[40vh] landscape:min-h-[65vh]'>
+          <div className='flex h-full flex-col justify-items-center gap-y-6 p-4 text-center sm:m-0 sm:w-7/12 sm:p-0 sm:text-start lg:w-5/12 lg:pl-4 xl:pl-12 2xl:pl-24'>
+            <h1 className='text-2xl sm:text-[1.7rem] lg:text-3xl xl:text-4xl 2xl:text-5xl'>
+              Projeto <IoInfiniteSharp className='mb-1 inline text-3xl sm:mb-2 sm:text-4xl lg:text-4xl xl:text-5xl 2xl:text-6xl' /> Infinity
             </h1>
-            <h3 className='break-words text-sm sm:text-base'>
+            <h3 className='break-words text-sm lg:text-base'>
               Aplicação para gestão de investimentos
               <br />
               Desenvolvido por{' '}
-              <a href='https://www.linkedin.com/in/tiagovinhanova/' target='_blank' rel='noreferrer noopener' className='text-custom-tealblue-hl underline underline-offset-4'>
+              <a href='https://www.linkedin.com/in/tiagovinhanova/' target='_blank' rel='noreferrer noopener' className='whitespace-nowrap text-custom-tealblue-hl underline underline-offset-4'>
                 Tiago Vinhanova
                 <FaLinkedin className='ml-2 inline text-custom-tealblue-hl' />
               </a>
@@ -49,35 +49,35 @@ const LoginPage: FC = () => {
             <GoogleButton onClick={handleGoogleSignIn} className='scale-90 self-center sm:scale-100 sm:self-start' />
           </div>
           <div className='-mt-4 sm:-mt-12 sm:w-7/12'>
-            <img src='../mockups.png' alt='Infinity App Phone, Tablet and PC Mockups' />
+            <img src='../mockups.png' alt='Infinity App Phone, Tablet and PC Mockups with example data' />
           </div>
         </div>
         <div
-          className='grid min-h-[65vh] w-full grid-cols-1 grid-rows-3 items-start justify-items-center gap-y-2 bg-[#1d1e20] p-8
-          sm:h-[40vh] sm:min-h-[0vh] sm:grid-cols-3 sm:grid-rows-1 sm:items-center sm:gap-y-8 sm:p-0 sm:px-16'
+          className='grid min-h-[65vh] w-full grid-cols-1 grid-rows-3 items-start justify-items-start gap-y-4 overflow-hidden bg-[#1d1e20] p-8 sm:min-h-[40vh] md:px-16
+          lg:h-[40vh] lg:min-h-[0vh] lg:grid-cols-3 lg:grid-rows-1 lg:items-center lg:justify-items-center lg:gap-y-8 lg:p-0 lg:px-16'
         >
-          <div className='h-[20vh] space-y-4 lg:mx-4 xl:mx-20'>
-            <FaFolderTree className='min-w-auto min-h-[30px] sm:text-2xl xl:text-3xl' />
-            <h4 className='text-base sm:text-lg xl:text-xl'>Tudo na mesma aplicação</h4>
+          <div className='space-y-4 lg:mx-4 xl:mx-12 2xl:mx-20'>
+            <FaFolderTree className='h-[30px] w-auto sm:text-2xl xl:text-3xl' />
+            <h4 className='text-base lg:text-lg xl:text-xl'>Tudo na mesma aplicação</h4>
             <h5 className='whitespace-normal text-sm font-light xl:text-base'>Adicione múltiplos tipos de ativos no mesmo Portfólio, como Ações e Criptomoedas</h5>
           </div>
-          <div className='h-[20vh] space-y-4 lg:mx-4 xl:mx-20'>
-            <MdAccessTime className='min-w-auto -ml-1 min-h-[30px] text-xl sm:text-3xl xl:text-4xl' />
-            <h4 className='text-base sm:text-lg xl:text-xl'>Atualizado em tempo real</h4>
+          <div className='space-y-4 lg:mx-4 xl:mx-12 2xl:mx-20'>
+            <MdAccessTime className='-ml-1 h-[30px] w-auto scale-110 text-xl sm:text-3xl xl:text-4xl' />
+            <h4 className='text-base lg:text-lg xl:text-xl'>Atualizado em tempo real</h4>
             <h5 className='whitespace-normal text-sm font-light xl:text-base'>Valor de Ações e Criptomoedas atualizado ao segundo</h5>
           </div>
-          <div className='h-[20vh] space-y-4 lg:mx-4 xl:mx-20'>
-            <FaChartPie className='min-w-auto -ml-0.5 min-h-[30px] text-xl sm:text-2xl xl:text-3xl' />
-            <h4 className='text-base sm:text-lg xl:text-xl'>Estatísticas</h4>
+          <div className='space-y-4 lg:mx-4 xl:mx-12 2xl:mx-20'>
+            <FaChartPie className='-ml-0.5 h-[30px] w-auto text-xl sm:text-2xl xl:text-3xl' />
+            <h4 className='text-base lg:text-lg xl:text-xl'>Estatísticas</h4>
             <h5 className='whitespace-normal text-sm font-light xl:text-base'>Gráficos representativos da distribuição do seu Portfólio</h5>
           </div>
         </div>
-        <div className='flex h-[40vh] w-full justify-center p-8'>
-          <Carousel pauseOnHover className='h-full w-full justify-center xs:w-8/12 sm:w-6/12 2xl:w-4/12' leftControl={<FaChevronLeft className='relative -left-10 text-2xl text-custom-tealblue-hl sm:-left-20' />} rightControl={<FaChevronRight className='relative -right-10 text-2xl text-custom-tealblue-hl sm:-right-20' />}>
-            <img src='../crypto-vs-stocks-piechart.png' alt='...' />
-            <img src='../new-asset.png' alt='...' />
-            <img src='../crypto-stocks-piechart.png' alt='...' />
-            <img src='../list.png' alt='...' />
+        <div className='flex w-full justify-center p-8 portrait:h-[30vh] landscape:h-[40vh]'>
+          <Carousel pauseOnHover className='h-full w-full justify-center xs:w-8/12 md:w-6/12 2xl:w-4/12' leftControl={<FaChevronLeft className='relative -left-10 text-2xl sm:-left-20' />} rightControl={<FaChevronRight className='relative -right-10 text-2xl sm:-right-20' />}>
+            <img className='lg:w-10/12' src='../crypto-vs-stocks-piechart.png' alt='All Assets and Crypto vs Stocks PieCharts Example' />
+            <img src='../new-asset.png' alt='New Asset Page Example' />
+            <img className='lg:w-10/12' src='../crypto-stocks-piechart.png' alt='Stocks and Crypto PieCharts Example' />
+            <img src='../list.png' alt='List of all Assets with 24h percent change Example' />
           </Carousel>
         </div>
         <div className='mx-12 flex h-[10vh] w-full items-center justify-center bg-[#1d1e20] text-sm sm:text-base'>
