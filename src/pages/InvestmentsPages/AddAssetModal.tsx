42ecea94fc2e642b65350ecc3d1bd142f27d2f52
Vinhanova <tiagovinhanova@gmail.com>
2024-04-17 10:56:23 +0100

    QOL AddAsset Changes

--- src/pages/InvestmentsPages/AddAssetModal.tsx
+++ src/pages/InvestmentsPages/AddAssetModal.tsx
@@ -23,11 +23,6 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
   const { borderPosition, mouseDownHandler } = useDraggHandler()
   const { user } = useUserAuth()
   const [symbol, setSymbol] = useState<string>('')
-  const [initialSearchLimit, setInitialSearchLimit] = useState<number>(3)
-  const [maxSearchLimit, setMaxSearchLimit] = useState<number>(9)
-  const [searchLimit, setSearchLimit] = useState<number>(initialSearchLimit)
-  const [searchList, setSearchList] = useState<any[]>([])
-  const [displayedSearchList, setDisplayedSearchList] = useState<any[3]>([1, 2, 3, 4])
   const [placeholder, setPlaceholder] = useState<{ symbol: { stock: string; cryptocurrency: string }; name: { stock: string; cryptocurrency: string } }>({
     symbol: {
       stock: 'Exemplo: AAPL',
@@ -45,6 +40,30 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
     type: 'stock',
     state: 'purchased'
   })
+
+  // #region isStock & isCryptocurrency
+
+  const [isStock, setIsStock] = useState<boolean>(false)
+  const [isCryptocurrency, setIsCryptocurrency] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (asset.type === 'stock') setIsStock(true)
+    else setIsStock(false)
+  }, [asset])
+
+  useEffect(() => {
+    if (asset.type === 'cryptocurrency') setIsCryptocurrency(true)
+    else setIsCryptocurrency(false)
+  }, [asset])
+
+  // #endregion
+
+  // #region Search Functionality
+
+  const [searchList, setSearchList] = useState<any[]>([])
+  const [initialSearchLimit, setInitialSearchLimit] = useState<number>(3)
+  const [maxSearchLimit, setMaxSearchLimit] = useState<number>(9)
+  const [searchLimit, setSearchLimit] = useState<number>(initialSearchLimit)
 
   const { data: cryptoData, error: cryptoError, isLoading: cryptoIsLoading } = useAxios<any>(`https://finnhub.io/api/v1/crypto/symbol?exchange=coinbase&token=${import.meta.env.VITE_FINNHUB_API_KEY}`)
 
@@ -81,7 +100,7 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
     if (addAssetModal) closeEditAssetModal()
   }, [addAssetModal])
 
-  // Symbol validation:
+  // Symbol Validation
 
   const inputRef = useRef<HTMLInputElement>(null)
   const [isSymbolValid, setIsSymbolValid] = useState(false)
@@ -93,12 +112,19 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
   useEffect(() => {
     if (!inputRef.current) return
 
+    if (!isCryptocurrency) {
+      inputRef.current.setCustomValidity('')
+      return
+    }
+
     if (isSymbolValid) {
       inputRef.current.setCustomValidity('')
     } else {
       inputRef.current.setCustomValidity('Símbolo não existe, selecione um por favor.\nExemplo: BTC-USD')
     }
-  }, [isSymbolValid])
+  }, [isSymbolValid, isCryptocurrency])
+
+  // #endregion
 
   return (
     <div className={addAssetModal ? 'absolute min-h-full min-w-full bg-custom-jet md:static md:flex md:min-w-min md:bg-custom-dark-jet' : 'hidden'}>
@@ -111,23 +137,36 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
                   `}
         style={{ width: borderPosition }}
       >
-        <div
-          className='absolute top-0 right-0 z-30 w-fit cursor-pointer p-2 text-3xl md:-left-1 md:top-1/2 md:-ml-4 md:p-0'
-          onClick={() => {
-            setAddAssetModal(false)
-          }}
-        >
+        <div className='absolute top-0 right-0 z-30 w-fit cursor-pointer p-2 text-3xl md:-left-1 md:top-1/2 md:-ml-4 md:p-0' onClick={() => setAddAssetModal(false)}>
           <MdClose className='md:hidden' />
           <IoIosArrowDropright className='hidden rounded-full bg-custom-dark-jet md:block' />
         </div>
         <form className='mt-6 flex flex-col gap-4 text-left [&_p]:mb-1' onSubmit={addAsset}>
           <div>
             <p>Tipo:</p>
-            <input checked={asset.type === 'stock'} id='add-stock-radio' type='radio' name='type' value='stock' className='mr-1.5 mb-0.5 scale-110 cursor-pointer text-custom-tealblue focus:ring-0 focus:ring-offset-0' onChange={e => setAsset({ ...asset, type: e.target.value } as typeof asset)} required />
+            <input //
+              checked={isStock}
+              id='add-stock-radio'
+              type='radio'
+              name='type'
+              value='stock'
+              className='mr-1.5 mb-0.5 scale-110 cursor-pointer text-custom-tealblue focus:ring-0 focus:ring-offset-0'
+              onChange={e => setAsset({ ...asset, type: e.target.value } as typeof asset)}
+              required
+            />
             <label htmlFor='add-stock-radio' className='mr-8 cursor-pointer focus:bg-transparent'>
               Ações
             </label>
-            <input checked={asset.type === 'cryptocurrency'} id='add-cryptocurrency-radio' type='radio' name='type' value='cryptocurrency' className='mr-1.5 mb-0.5 scale-110 cursor-pointer text-custom-tealblue focus:ring-0 focus:ring-offset-0' onChange={e => setAsset({ ...asset, type: e.target.value } as typeof asset)} required />
+            <input //
+              checked={isCryptocurrency}
+              id='add-cryptocurrency-radio'
+              type='radio'
+              name='type'
+              value='cryptocurrency'
+              className='mr-1.5 mb-0.5 scale-110 cursor-pointer text-custom-tealblue focus:ring-0 focus:ring-offset-0'
+              onChange={e => setAsset({ ...asset, type: e.target.value } as typeof asset)}
+              required
+            />
             <label htmlFor='add-cryptocurrency-radio' className='cursor-pointer'>
               Criptomoedas
             </label>
@@ -135,11 +174,19 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
           <div>
             <p>Símbolo</p>
             <div className='flex'>
-              <input ref={inputRef} type='text' className='w-full rounded py-1 px-2 text-sm uppercase text-custom-jet placeholder:opacity-60 invalid:text-red-500 focus:ring-2 focus:ring-custom-tealblue lg:text-base' placeholder={placeholder.symbol[asset.type]} value={symbol} onChange={e => handleSymbolChange(e)} required />
-              {symbol != '' && <div className={'-ml-8 mt-1.5 text-xl ' + (isSymbolValid ? 'text-green-500' : 'text-red-500')}>{isSymbolValid ? <FaCheck /> : <MdOutlineWarningAmber />}</div>}
+              <input ref={inputRef} type='text' className='peer w-full rounded py-1 px-2 text-sm uppercase text-custom-jet placeholder:opacity-60 invalid:text-red-500 focus:ring-2 focus:ring-custom-tealblue lg:text-base' placeholder={placeholder.symbol[asset.type]} value={symbol} onChange={e => handleSymbolChange(e)} required />
+              {symbol !== '' && (
+                <div className='-ml-8 mt-1.5 text-xl peer-valid:text-green-500 peer-invalid:text-red-500'>
+                  {isSymbolValid ? ( //
+                    <FaCheck />
+                  ) : (
+                    <MdOutlineWarningAmber />
+                  )}
+                </div>
+              )}
             </div>
           </div>
-          {asset.type === 'cryptocurrency' && searchList.length > 0 && (
+          {isCryptocurrency && searchList.length > 0 && (
             <div className='flex flex-col items-center gap-y-2 px-12'>
               {searchList.map((cryptoItem: any) => (
                 <button type='button' key={cryptoItem?.displaySymbol} className='!m-0 w-full cursor-pointer rounded bg-custom-jet px-2 py-1 text-center' onClick={() => setSymbol(cryptoItem.displaySymbol.replace('/', '-'))}>
@@ -148,7 +195,11 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
               ))}
               {searchList.length >= searchLimit && (
                 <button type='button' key={1} className='flex w-min cursor-pointer justify-center px-3 py-1 font-semibold' onClick={() => setSearchLimit(searchLimit => (searchLimit === initialSearchLimit ? maxSearchLimit : initialSearchLimit))}>
-                  {searchLimit === initialSearchLimit ? <FiMaximize2 /> : <FiMinimize2 />}
+                  {searchLimit === initialSearchLimit ? ( //
+                    <FiMaximize2 />
+                  ) : (
+                    <FiMinimize2 />
+                  )}
                 </button>
               )}
             </div>
@@ -159,7 +210,16 @@ const AddAssetModal: FC<Props> = ({ addAssetModal, setAddAssetModal }) => {
           </div>
           <div>
             <p>Quantidade</p>
-            <input className='w-full rounded py-1 px-2 text-sm text-custom-jet focus:ring-2 focus:ring-custom-tealblue lg:text-base' value={asset.quantity} type='number' onChange={e => setAsset({ ...asset, quantity: +e.target.value })} inputMode='numeric' min={asset.type === 'stock' ? 1 : 0.000000001} step={asset.type === 'stock' ? 1 : 0.000000001} required />
+            <input //
+              className='w-full rounded py-1 px-2 text-sm text-custom-jet focus:ring-2 focus:ring-custom-tealblue lg:text-base'
+              value={asset.quantity}
+              type='number'
+              onChange={e => setAsset({ ...asset, quantity: +e.target.value })}
+              inputMode='numeric'
+              min={isStock ? 1 : 0.000000001}
+              step={isStock ? 1 : 0.000000001}
+              required
+            />
           </div>
           {/* <div>
           <p>Watchlist</p>
