b791bf07e1ec1856f710fa41da479762124a2c04
Vinhanova <tiagovinhanova@gmail.com>
2024-04-27 13:59:28 +0100

    API List Errors Safe Check

--- src/pages/InvestmentsPages/ListPage.tsx
+++ src/pages/InvestmentsPages/ListPage.tsx
@@ -119,10 +119,31 @@ const InvestmentsPage: FC = () => {
                           </th>
                           <td className='bg-white/5 px-[3px] py-[7px] text-right text-gray-300 lg:px-2 lg:py-3 xl:px-6 xl:py-4'>{stock.price.toFixed(stock.price < 1 ? 3 : 2)} $</td>
                           <td className='px-[3px] py-[7px] text-right lg:px-2 lg:py-3 xl:px-6 xl:py-4'>
-                            <span className={stock.changePercent === null ? '' : stock.changePercent > 0 ? 'text-green-500' : 'text-red-500'}>{stock.change ? (stock.change > 0 ? '+' : '') + `${stock.change?.toFixed(2)} (${(stock.changePercent > 0 ? '+' : '') + stock.changePercent?.toFixed(2)}%)` : `${(stock.changePercent > 0 ? '+' : '') + stock.changePercent?.toFixed(2)}%`}</span>
+                            <span className={stock.changePercent === null ? '' : stock.changePercent > 0 ? 'text-green-500' : 'text-red-500'}>
+                              {stock.changePercent !== null //
+                                ? stock.change //
+                                  ? (stock.change > 0 ? '+' : '') +
+                                    `${stock.change?.toFixed(2)} (${
+                                      (stock.changePercent > 0 //
+                                        ? '+'
+                                        : '') + stock.changePercent?.toFixed(2)
+                                    }%)`
+                                  : `${
+                                      (stock.changePercent > 0 //
+                                        ? '+'
+                                        : '') + (stock.price - stock.price / (stock.changePercent / 100 + 1))?.toFixed(2)
+                                    } (${stock.changePercent?.toFixed(2)}%)`
+                                : 'Erro API *'}
+                            </span>
                           </td>
                           <td className={`hidden bg-white/5 px-[3px] py-[7px] text-right sm:table-cell lg:px-2 lg:py-3 xl:px-6 xl:py-4 ${stock.changePercent === null ? `` : stock.changePercent > 0 ? 'text-green-500' : 'text-red-500'}`}>{stock.change ? `${(stock.change * userTickersData![stock.id].quantity * exchangeRateInfoData?.c).toFixed(2)} €` : '... *'}</td>
-                          <td className='hidden px-[3px] py-[7px] text-right text-gray-200 sm:table-cell lg:px-2 lg:py-3 xl:px-6 xl:py-4'>{toFixed(stock.price * userTickersData![stock.id].quantity * exchangeRateInfoData?.c, 2)} €</td>
+                          <td className='hidden px-[3px] py-[7px] text-right text-gray-200 sm:table-cell lg:px-2 lg:py-3 xl:px-6 xl:py-4'>
+                            {
+                              //
+                              toFixed(stock.price * userTickersData![stock.id].quantity * (exchangeRateInfoData?.c ? exchangeRateInfoData.c : 0.932), 2)
+                            }{' '}
+                            €
+                          </td>
                           <td>
                             <Dropdown
                               label=''
